b1f284cc7a2835017d895b7438bfa2830582bd4c
Yurickh <yurick.hauschild@gmail.com>
2016-09-28 17:51:08 -0300

    Enclosed navbar functionality to the directive.
    Installed jasmine for posterior testing.
    Scroll resizing is working, next step is adjusting the sass to change colors.

--- scripts/directives/navbar.js
+++ scripts/directives/navbar.js
@@ -1,5 +1,5 @@
 export default
-function navbar()
+function navbar($document, $window)
 {
     return {
         restrict: 'E',
@@ -7,7 +7,7 @@ function navbar()
             menu: '='
         },
         templateUrl: 'directives/navbar',
-        link: function(scope)
+        link: function(scope, element, attrs)
         {
             scope.menu.links = scope.menu.links
                 .map((element) => {
@@ -16,6 +16,20 @@ function navbar()
                         name: element[1]
                     }
                 })
+
+            element.addClass('grid grid-fluid')
+
+            $document.on('scroll', () => {
+                console.log($window.pageYOffset)
+                if($window.pageYOffset >= 51)
+                {
+                    if(! element.hasClass('affix')) element.addClass('affix')
+                } else {
+                    if(element.hasClass('affix')) element.removeClass('affix')
+                }
+            })
         }
     }
 }
+
+navbar.$inject = ['$document', '$window']
--- scripts/home/home.controller.js
+++ scripts/home/home.controller.js
@@ -3,18 +3,6 @@ import angular from 'angular'
 export default
 function HomeCtrl($scope, $document)
 {
-    $document.on('scroll', () => {
-
-        if($document.scrollTop() >= 51)
-        {
-            let navbar = angular.element('.navbar')
-            if(! navbar.hasClass('affix')) navbar.addClass('affix')
-        } else {
-            let navbar = angular.element('.navbar')
-            if(navbar.hasClass('affix')) navbar.removeClass('affix')
-        }
-    })
-
     $scope.navbarContent = {
         banner: 'home',
         links: [

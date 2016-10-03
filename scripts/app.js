f2469eba29e5923298f195b3bb5af7963566d02a
Yurickh <yurick.hauschild@gmail.com>
2016-10-03 17:24:08 -0300

    Fixing dependencies.

--- scripts/app.js
+++ scripts/app.js
@@ -4,10 +4,14 @@
 
 import angular from 'angular'
 import uirouter from 'angular-ui-router/release/angular-ui-router.js'
+import duScroll from 'angular-scroll/angular-scroll.js'
 
 import directives from 'directives'
 import routing from 'app.routes'
 import home from 'home'
 
-angular.module('havendale', [uirouter, home, directives])
+angular.module('havendale', [uirouter,
+                             duScroll.name,
+                             home,
+                             directives])
     .config(routing)
